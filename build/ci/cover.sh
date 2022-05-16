fc668eef9b41e6782c8f86283ea72342711ad78c
Washington Rodrigues <washingtonjunio16@gmail.com>
2022-05-16 13:05:41 -0300

    feat: add coverage CI

---
+++ build/ci/cover.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+COVER=$(go tool cover -func cover.out | grep total | egrep -o [0-9.]+)
+TARGET_COVER="100.0"
+if [ $COVER = $TARGET_COVER ]; then
+  exit 0
+else
+  echo "Expected coverage $TARGET_COVER current $COVER"
+  exit 1
+fi
