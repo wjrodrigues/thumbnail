fb2e0c2e10c9b20ca7ffdc4f81382256b3682e38
Washington Rodrigues <washingtonjunio16@gmail.com>
2022-05-16 14:32:39 -0300

    other: test coverage

--- build/ci/cover.sh
+++ build/ci/cover.sh
@@ -3,6 +3,7 @@
 COVER=$(go tool cover -func cover.out | grep total | egrep -o [0-9.]+)
 TARGET_COVER="100.0"
 if [ $COVER = $TARGET_COVER ]; then
+  echo "Success coverage $COVER"
   exit 0
 else
   echo "Expected coverage $TARGET_COVER current $COVER"
--- pkg/thumbnail.go
+++ pkg/thumbnail.go
@@ -3,3 +3,7 @@ package thumbnail
 func run() bool {
 	return true
 }
+
+func v() int {
+	return 1
+}
